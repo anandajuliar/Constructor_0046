610464f8f8be365638614e8dbe115d95f1d0aa17
anandajuliar <anandajulia001@gmail.com>
2024-05-14 18:15:53 +0700

    void

--- Constructor2/Constructor2.cpp
+++ Constructor2/Constructor2.cpp
@@ -25,3 +25,16 @@ public:
 };
 
 int mahasiswa::nim = 0;
+
+
+void mahasiswa::setId()
+{
+    id = ++nim;
+}
+
+void mahasiswa::printAll()
+{
+    cout << "ID     = " << id << endl;
+    cout << "Nama   = " << nama << endl;
+    cout << endl;
+}
