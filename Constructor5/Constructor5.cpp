926a8e7de1d435d6a17db487df0a8aefbfa0454e
anandajuliar <anandajulia001@gmail.com>
2024-05-14 18:36:40 +0700

    commit

--- Constructor5/Constructor5.cpp
+++ Constructor5/Constructor5.cpp
@@ -14,6 +14,22 @@ public:
     void isiData();
 };
 
+// Definisi member Function
+angka::angka(int i)  // Constructor
+{
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
+
+angka::~angka()       // Destructor
+{
+    cout << endl;
+    cetakData();
+    delete[]arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
 
 
 int main()
