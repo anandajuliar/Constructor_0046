11130ab1b644cf52f9444046e63cac457a87b189
anandajuliar <anandajulia001@gmail.com>
2024-05-14 18:37:02 +0700

    void

--- Constructor5/Constructor5.cpp
+++ Constructor5/Constructor5.cpp
@@ -30,7 +30,24 @@ angka::~angka()       // Destructor
     cout << "Alamat Array Sudah Dilepaskan" << endl;
 }
 
+void angka::cetakData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
 
+void angka::isiData()
+{
+    for (int i = 1; 1 <= panjang; i++)
+    {
+        cout << i << " = ";
+        cin >> arr[i];
+    }
+
+    cout << endl;
+}
 
 int main()
 {
