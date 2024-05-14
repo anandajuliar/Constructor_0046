d12536d5131b8a11aed30975272f1095e027cff1
anandajuliar <anandajulia001@gmail.com>
2024-05-14 18:36:06 +0700

    class angka int main

--- Constructor5/Constructor5.cpp
+++ Constructor5/Constructor5.cpp
@@ -14,40 +14,7 @@ public:
     void isiData();
 };
 
-// Definisi member Function
-angka::angka(int i)  // Constructor
-{
-    panjang = i;
-    arr = new int[i];
-    isiData();
-}
 
-angka::~angka()       // Destructor
-{
-    cout << endl;
-    cetakData();
-    delete[]arr;
-    cout << "Alamat Array Sudah Dilepaskan" << endl;
-}
-
-void angka::cetakData()
-{
-    for (int i = 1; i <= panjang; i++)
-    {
-        cout << i << " = " << arr[i] << endl;
-    }
-}
-
-void angka::isiData()
-{
-    for (int i = 1; 1 <= panjang; i++)
-    {
-        cout << i << " = ";
-        cin >> arr[i];
-    }
-
-    cout << endl;
-}
 
 int main()
 {
