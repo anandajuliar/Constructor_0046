d32fe3cba76bfb74f31f1564aaa009b18c1f0141
anandajuliar <anandajulia001@gmail.com>
2024-05-14 18:35:07 +0700

    class buku

--- Constructor4/Constructor4.cpp
+++ Constructor4/Constructor4.cpp
@@ -17,10 +17,3 @@ public:
     }
 } bukunya;
 
-int main()
-{
-    // bukunya.setJudul("Matematika");
-    // cout << bukunya.getJudul();
-    cout << bukunya.setJudul("Matematika").getJudul(); // chain function calls
-    return 0;
-}
